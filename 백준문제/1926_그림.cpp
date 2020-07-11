c821e298779ab80572c209368aca78a170d34879
Changhwan Son <48404869+Changhwan-Son@users.noreply.github.com>
2020-07-11 14:26:02 +0900

    0711 알고리즘
    부스트캠프 코테 대비

---
+++ 백준문제/1926_그림.cpp
@@ -0,0 +1,94 @@
+/*
+1196번 그림 
+BFS
+
+문제
+어떤 큰 도화지에 그림이 그려져 있을 때, 그 그림의 개수와, 그 그림 중 넓이가 가장 넓은 것의 넓이를 출력하여라. 단, 그림이라는 것은 1로 연결된 것을 한 그림이라고 정의하자. 가로나 세로로 연결된 것은 연결이 된 것이고 대각선으로 연결이 된 것은 떨어진 그림이다. 그림의 넓이란 그림에 포함된 1의 개수이다.
+
+입력
+첫째 줄에 도화지의 세로 크기 n(1 ≤ n ≤ 500)과 가로 크기 m(1 ≤ m ≤ 500)이 차례로 주어진다. 두 번째 줄부터 n+1 줄 까지 그림의 정보가 주어진다. (단 그림의 정보는 0과 1이 공백을 두고 주어지며, 0은 색칠이 안된 부분, 1은 색칠이 된 부분을 의미한다)
+
+출력
+첫째 줄에는 그림의 개수, 둘째 줄에는 그 중 가장 넓은 그림의 넓이를 출력하여라. 단, 그림이 하나도 없는 경우에는 가장 넓은 그림의 넓이는 0이다.
+
+예제 입력 1 
+6 5
+1 1 0 1 1
+0 1 1 0 0
+0 0 0 0 0
+1 0 1 1 1
+0 0 1 1 1
+0 0 1 1 1
+
+예제 출력 1 
+4
+9
+*/
+
+#include <iostream>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+int main(){
+	int m;			// 가로
+	int n;			// 세로
+	int max = 0;
+	int num = 0;
+	
+	bool visit[501][501];
+	int board[501][501];
+	
+	int dx[4] = {1,0,-1,0};
+	int dy[4] = {0,1,0,-1};
+	
+	
+	
+	
+	// 세로, 가로, 그림 정보 입력받기
+	cin >> n >> m;
+	for(int i = 0; i < n; i++)
+		for(int j = 0 ; j < m; j++)
+			cin >> board[i][j];
+	
+	
+	
+	for(int i = 0; i < n; i++){
+		for(int j = 0 ; j < m; j++){
+			if(visit[i][j] == 1 || board[i][j] == 0)
+				continue;
+			
+			num++;
+			visit[i][j] = 1;
+			int count = 0;
+			
+			queue<pair<int, int>> visitQueue;
+			visitQueue.push({i,j});
+			
+			while(!visitQueue.empty()){
+				pair<int, int> cur = visitQueue.front();
+				visitQueue.pop();
+				count++;
+				
+				for(int k = 0 ; k < 4; k++){
+					int px = cur.first + dx[k];
+					int py = cur.second + dy[k];
+					if(px < 0 || px >= n || py < 0 || py > m)
+						continue;
+					if(board[px][py] != 1 || visit[px][py] == 1)
+						continue;
+
+					visitQueue.push({px, py});
+					visit[px][py] = 1;
+				}
+			}
+			
+			if (count > max)
+				max = count;
+			
+		}
+	}
+	
+	cout << num << "\n" << max;
+}
---
+++ 백준문제/2178_미로탐색.cpp
@@ -0,0 +1,90 @@
+/*
+2178번 미로탐색
+BFS
+
+문제
+N×M크기의 배열로 표현되는 미로가 있다.
+
+1	0	1	1	1	1
+1	0	1	0	1	0
+1	0	1	0	1	1
+1	1	1	0	1	1
+미로에서 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸을 나타낸다. 이러한 미로가 주어졌을 때, (1, 1)에서 출발하여 (N, M)의 위치로 이동할 때 지나야 하는 최소의 칸 수를 구하는 프로그램을 작성하시오. 한 칸에서 다른 칸으로 이동할 때, 서로 인접한 칸으로만 이동할 수 있다.
+
+위의 예에서는 15칸을 지나야 (N, M)의 위치로 이동할 수 있다. 칸을 셀 때에는 시작 위치와 도착 위치도 포함한다.
+
+입력
+첫째 줄에 두 정수 N, M(2 ≤ N, M ≤ 100)이 주어진다. 다음 N개의 줄에는 M개의 정수로 미로가 주어진다. 각각의 수들은 붙어서 입력으로 주어진다.
+
+출력
+첫째 줄에 지나야 하는 최소의 칸 수를 출력한다. 항상 도착위치로 이동할 수 있는 경우만 입력으로 주어진다.
+
+예제 입력 1 
+4 6
+101111
+101010
+101011
+111011
+
+예제 출력 1 
+15
+
+
+*/
+
+#include <iostream>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+int main(){
+	int n;
+	int m;
+	
+	int count = 0;
+	int max = 0;
+	
+	int dx[4] = {1,0,-1,0};
+	int dy[4] = {0,1,0,-1};
+	
+	string temp[101];
+	int miro[101][101];
+	int visit[101][101] = {0};
+	
+	cin >> n >> m;
+	for(int i = 0; i < n; i++)
+		cin >> temp[i];
+	
+	for(int i = 0 ; i < n; i++){
+		for(int j = 0; j < m; j++){
+			miro[i][j] = temp[i][j];
+		}
+	}
+	
+	
+	queue<pair<int, int>> visitQueue;
+	visitQueue.push({0,0});
+
+	while(!visitQueue.empty()){
+		pair<int, int> cur = visitQueue.front();
+		visitQueue.pop();
+
+
+		for(int k = 0 ; k < 4; k++){
+			int px = cur.first + dx[k];
+			int py = cur.second + dy[k];
+
+			if(px < 0 || px >= n || py < 0 || py>= m)
+				continue;
+
+			if(miro[px][py] == '0' || visit[px][py] > 0)
+				continue;
+			visitQueue.push({px, py});
+			visit[px][py] = visit[cur.first][cur.second] + 1;
+		}
+			
+	}
+	
+	cout << visit[n-1][m-1] + 1;
+}
