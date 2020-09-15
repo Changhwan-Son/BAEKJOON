595cf0b2b87f2ad4799d92ce020b87666f7ee2c2
Changhwan Son <48404869+Changhwan-Son@users.noreply.github.com>
2020-09-15 14:49:56 +0900

    Update 1629_곱셈.cpp

--- 백준문제/1629_곱셈.cpp
+++ 백준문제/1629_곱셈.cpp
@@ -16,20 +16,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long ;
+
+
+ll pow(ll a, ll b, ll c){
+    if(b == 1) 
+        return a % c;
+
+    ll result = pow(a, b / 2, c);
+    result = result * result % c;
+    if(b % 2 == 0)
+        return result;
+    
+    return result * a % c;  
+}
 
 int main(){
-    int A;
-    int B;
-    int C;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll a, b, c;
+
+    cin >> a >> b >> c;
+    cout << pow(a, b, c);
     
-    cin >> A >> B >> C;
-    int result = A;
-    while(B--){
-        result = result % C;
-    }
-
-    cout << result;
-
     return 0;
 
 }
+
